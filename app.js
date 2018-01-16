bcbce1710e4351f62b35daacdf14c00d79645314
Slavik Manukyan <slavikmanukyan@gmail.com>
2018-01-16 11:58:04 +0400

    basic auth

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true
-}))
+}));
+
+function loginCheckMiddleware(req, res, next) {
+    if (req.session.user) {
+        res.redirect('/home');
+    } else {
+        next();
+    }
+}
+
 
 app.get('/', (req, res) => {
     console.log('request');
@@ -39,13 +48,84 @@ app.get('/', (req, res) => {
 
 app.get('/users/add', (req, res) => {
     res.render('addUser');
-})
+});
+
+app.get('/home', (req, res) => {
+    if (!req.session.user) {
+        res.redirect('/login');
+    } else {
+        res.render('userHome', { user: req.session.user });
+    }
+});
+
+app.get('/login',loginCheckMiddleware, (req,res) => {
+    const errors = Object.assign({}, req.session.errors);
+    req.session.errors = null;
+    res.render('login', { errors });
+});
+
+app.get('/signup',loginCheckMiddleware, (req, res) => {
+    const errors = Object.assign({}, req.session.errors);
+    req.session.errors = null;
+    res.render('signUp', { errors });
+});
 
 const users = [
-{
-    name: 'Bob'
-}
+
 ];
+
+// Auth
+
+
+app.post('/signup', (req, res) => {
+    req.checkBody('username').notEmpty().withMessage('username is required');
+    req.checkBody('password').notEmpty().withMessage('password is required').equals(req.body['r-password']).withMessage('passwords must be equal');
+
+    const errors = req.validationErrors();
+    if (errors) {
+        req.session.errors = errors;
+        res.redirect('/signup');
+    } else {
+        users.push({
+            username: req.body.username,
+            password: req.body.password,
+        });
+        req.session.user = req.body.username;
+        req.session.errors = null;
+        res.redirect('/home');
+    }
+});
+
+app.post('/login', (req, res) => {
+    req.checkBody('username').notEmpty().withMessage('username is required');
+    req.checkBody('password').notEmpty().withMessage('password is required');
+
+    const errors = req.validationErrors();
+    if (errors) {
+        req.session.errors = errors;
+        res.redirect('/login');
+    } else {
+        const user = users.find((user) => user.username === req.body.username && user.password === req.body.password);
+        if (!user) {
+            req.session.errors = [{
+                msg: 'wrong username or password',
+            }];
+            res.redirect('/login');
+        } else {
+            req.session.user = user.username;
+            req.session.errors = null;
+            res.redirect('/home');
+        }
+    }
+});
+
+app.post('/signout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/login');
+    });
+});
+
+// Auth
 
 app.get('/api/user', (req, res) => {
     res.json({
